7019cd4ef2b1821f9a65dbf46f8be7ec43407543
anirudha-ani <anirudhaprasun@gmail.com>
2020-07-08 05:47:22 +0600

    Registering user and saving it to MongoDB

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const brcypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+
+// MongoDB schema
+const User = require('../../models/Users');
 
 // @route POST api/users
 // @desc Register user
@@ -15,13 +20,51 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('User registered');
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // Encrypt password
+      const salt = await brcypt.genSalt(10);
+
+      user.password = await brcypt.hash(password, salt);
+
+      await user.save();
+
+      // Return json web token
+      res.send('User registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
